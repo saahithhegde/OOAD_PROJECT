aabcf40e14b2ca2bf07561e8d3748a770899f07c
saahithhegde <saahithhegde@yahoo.co.in>
2020-10-03 21:14:24 -0500

    update database properties and tested connection

--- src/main/java/com/example/cash4books/cash4books/entity/usersEntity.java
+++ src/main/java/com/example/cash4books/cash4books/entity/usersEntity.java
@@ -1,4 +1,18 @@
 package com.example.cash4books.cash4books.entity;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "users")
 public class usersEntity {
+
+    @Id
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
