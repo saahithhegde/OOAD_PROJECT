ac0d087b940e1f51d3e826cb53651d2316209ded
saahithhegde <saahithhegde@yahoo.co.in>
2020-10-03 19:05:55 -0500

    The initial SpringBoot Application is set up

---
+++ src/main/java/com/example/cash4books/cash4books/Cash4booksApplication.java
@@ -0,0 +1,13 @@
+package com.example.cash4books.cash4books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cash4booksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cash4booksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cash4books/cash4books/Cash4booksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cash4books.cash4books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cash4booksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
