66c71c94f8b1f7e461f9dbce6c22d728722a8e89
saahithhegde <saahithhegde@yahoo.co.in>
2020-10-24 09:08:20 -0500

    UserProfile Details Almost Implemented new

---
+++ src/main/java/com/example/cash4books/cash4books/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.cash4books.cash4books.repository;
+
+import com.example.cash4books.cash4books.entity.Users;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<Users,String> {
+    Users findUserByEmail(String email);
+}
